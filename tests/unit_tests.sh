409dafdfaa3d43782238ec327ba5cd1063c03e75
Maxime Jobin <m@ximejobin.com>
2019-05-19 13:34:20 -0400

    Add script to launch all unit tests at once

---
+++ tests/unit_tests.sh
@@ -0,0 +1,4 @@
+../vendor/bin/phpunit --bootstrap ../vendor/autoload.php Po_Extractor_Test.php
+../vendor/bin/phpunit --bootstrap ../vendor/autoload.php Po_Merger_Test.php
+../vendor/bin/phpunit --bootstrap ../vendor/autoload.php Po_Test.php
+../vendor/bin/phpunit --bootstrap ../vendor/autoload.php Merger_Test.php
